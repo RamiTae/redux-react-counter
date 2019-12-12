0d177717767386f5c16fddfa1865d61197015db6
RamiTae <ramiTae.dev@gmail.com>
2019-12-13 00:02:58 +0900

    Buttons completed

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { increment, decrement } from "../actions";
 
-export default class Buttons extends Component {
+class Buttons extends Component {
   render() {
     return (
       <div>
-        <button>+</button>
-        <button>-</button>
+        <button onClick={this.props.onIncrement}>+</button>
+        <button onClick={this.props.onDecrement}>-</button>
       </div>
     );
   }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onIncrement: () => dispatch(increment()),
+    onDecrement: () => dispatch(decrement())
+  };
+};
+
+Buttons = connect(null, mapDispatchToProps)(Buttons);
+/**
+ * ! mapStateToProps: 보여지는 컴포넌트(state값이 display)의 경우
+ * ! mapDispatchToProps: 액션이 필요할 경우
+ */
+
+export default Buttons;
