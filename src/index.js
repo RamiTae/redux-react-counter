b3ea3b141735be5c3a3a930c7e503629fa1d67a3
RamiTae <ramiTae.dev@gmail.com>
2019-12-13 00:06:48 +0900

    redux-logger 사용

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createStore } from "redux";
+// import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import counterReducer from "./reducers";
 import { Provider } from "react-redux";
+import logger from "redux-logger"; //logger를 쓰기 위해 받아옴
 
-const store = createStore(counterReducer); //? reducer를 불러와야 함
+const store = createStore(counterReducer, applyMiddleware(logger));
+// const store = createStore(counterReducer); //? reducer를 불러와야 함
 
 ReactDOM.render(
   <Provider store={store}>
